76f56d1f0e28e5a71c09ed760df4fa019406067a
Brian <brian_@DESKTOP-GJH2BCP>
2023-08-03 18:35:24 -0400

    new commit

---
+++ my-profile.js
@@ -0,0 +1,6 @@
+/// Header with name
+console.log('Welcome to my page!')
+const nameHeader = document.createElement('h1#name')
+nameHeader.innerText = "Name here"
+const headerDiv = document.querySelector('div')
+headerDiv.append(nameHeader)
